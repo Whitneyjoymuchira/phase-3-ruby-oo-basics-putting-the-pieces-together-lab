f3ea89c28db3aac3e8c2d63b33673a9da2aff81e
Whitney Muchira <whitneymuchira@gmail.com>
2022-08-19 19:05:47 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
 
+   class Book
+    attr_accessor :page_count, :title, :author, :genre
+    def initialize (title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+   first_book = Book.new("Secret History")
+first_book.author = "Donna Tart"
+first_book.genre = "modern classics"
+first_book.page_count = 695
 
